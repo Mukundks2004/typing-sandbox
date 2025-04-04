09bea2e5982966806b51826e67827bb6d8d6e208
Mks <mukundsrinivasan0@gmail.com>
2025-04-04 23:48:07 +1100

    commented out terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import NavBar from "./components/NavBar";
 import Dropdown from "./components/Dropdown";
-import Dropdown2 from "./components/Dropdown2";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Dropdown></Dropdown>
-      {/* <Dropdown2></Dropdown2> */}
     </>
   );
 }
